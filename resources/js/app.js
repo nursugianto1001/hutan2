6d50fa056a8df716ea8fb1d4917ff12cdccbb910
alghazali <h1101231059@student.untan.ac.id>
2025-03-14 07:58:16 +0700

    menambahkan frontend di login

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,19 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const togglePassword = document.getElementById("togglePassword");
+    if (togglePassword) {
+        togglePassword.addEventListener("click", function () {
+            const password = document.getElementById("password");
+            if (password.type === "password") {
+                password.type = "text";
+            } else {
+                password.type = "password";
+            }
+        });
+    }
+});
+
+
